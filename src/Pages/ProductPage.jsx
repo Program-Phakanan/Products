a075866ab8c36c9a5889aed5fc686b157e732a38
Program-Phakanan <program.phakanan@gmail.com>
2025-01-30 13:44:02 +0700

    update API URL

--- src/Pages/ProductPage.jsx
+++ src/Pages/ProductPage.jsx
@@ -11,7 +11,7 @@ const [searchTerm, setSearchTerm] = useState("");
 useEffect(() => {
   const fetchProducts = async () => {
     try {
-      const response = await fetch("https://products-lc7wyxru-phakanans-projects.vercel.app/api/products");
+      const response = await fetch("http://localhost:8080/api/products");
       if (!response.ok) {
         throw new Error(`HTTP error! status: ${response.status}`);
       }
@@ -27,7 +27,7 @@ useEffect(() => {
 const addProduct = async (product) => {
   try {
     const response = await fetch(
-      "https://products-lc7wyxru-phakanans-projects.vercel.app/api/products",
+      "http://localhost:8080/api/products",
       {
         method: "POST",
         headers: {
@@ -50,7 +50,7 @@ const addProduct = async (product) => {
 const updateProduct = async (product, id) => {
   try {
     const response = await fetch(
-      `https://products-lc7wyxru-phakanans-projects.vercel.app/api/products/${id}`,
+      `http://localhost:8080/api/products/${id}`,
       {
         method: "PUT",
         headers: {
@@ -75,7 +75,7 @@ const updateProduct = async (product, id) => {
 const deleteProduct = async (id) => {
   try {
     const response = await fetch(
-      `https://products-lc7wyxru-phakanans-projects.vercel.app/api/products/${id}`,
+      `http://localhost:8080/api/products/${id}`,
       {
         method: "DELETE",
       }
